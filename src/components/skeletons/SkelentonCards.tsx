df7055d2caa8d2a6489ec32f5046366da336418c
Hasuro1797 <hasuro@gmail.com>
2024-06-23 12:52:40 -0500

    errores

--- src/components/skeletons/SkelentonCards.tsx
+++ src/components/skeletons/SkelentonCards.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@nextui-org/react'
 export default function SkelentonCards({hasPagination = false}: {hasPagination?: boolean}) {
   return (
     <section>
-      <ul className='grid grid-cols-1 justify-items-center bg-red-100 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+      <ul className='grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-6'>
         {
           [...Array(20).keys()].map(element => (
             <Skeleton key={element} className='aspect-square rounded-2xl w-full max-w-[300px]'/>
